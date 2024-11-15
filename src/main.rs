df131b90870359ecaee052126738d3fc38b26db6
Tawanda <ejmunongo@gmail.com>
2024-11-15 05:11:58 +0000

    working hello world

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,22 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello, World!";
+
 // dont mangle the function name
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // cast to raw pointer
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    // iterate over bytes of static HELLO byte string
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {
 
     }
